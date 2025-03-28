e7e4b312b60b7d214683f50ccee6836aa30938d6
Eddy <comnic@gurufin.com>
2025-03-28 16:05:26 +0900

    add ForEachStorage() interface

--- core/vm/interface.go
+++ core/vm/interface.go
@@ -78,6 +78,8 @@ type StateDB interface {
 
 	AddLog(*types.Log)
 	AddPreimage(common.Hash, []byte)
+
+	ForEachStorage(common.Address, func(common.Hash, common.Hash) bool) error
 }
 
 // CallContext provides a basic interface for the EVM calling conventions. The EVM
